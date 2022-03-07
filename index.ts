8ad2d7809f8c3b3788950c066f1b7909ce57c47c
Harminder Virk <virk.officials@gmail.com>
2022-03-07 09:31:54 +0530

    feat: initial working version

---
+++ index.ts
@@ -0,0 +1,12 @@
+/*
+ * @japa/preset-adonis
+ *
+ * (c) Harminder Virk <virk@adonisjs.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+export { assert } from '@japa/assert'
+export { specReporter } from '@japa/spec-reporter'
+export { runFailedTests } from '@japa/run-failed-tests'
